eab23affc2715ed47634f620245efcae57d8c946
Freeman Liu <llw599502537@gmail.com>
2023-02-12 03:57:12 +0800

    delete comment

--- classpath-replacer-core/src/main/java/com/freemanan/cr/core/anno/Action.java
+++ classpath-replacer-core/src/main/java/com/freemanan/cr/core/anno/Action.java
@@ -10,7 +10,6 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-// @Repeatable(ClasspathReplacer.class)
 public @interface Action {
 
     String[] value();
